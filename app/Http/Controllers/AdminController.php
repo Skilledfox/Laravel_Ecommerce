69e8a51797597bc84b409daef29550e6a4b6079d
shuvo <shuvosaha.saha87@gmail.com>
2018-09-22 03:59:52 +0600

    First Commit For My Laravel Ecommecre Project

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AdminController extends Controller
+{
+    public function login(Request $request){
+    	if ($request->isMethod('post')) {
+    		$data = $request->input();
+    		if (Auth::attempt(['email'=>$data['email'], 'password'=>$data['password'], 'admin'=>'1'])) {
+                return redirect('/admin/dashboard');
+    			//echo "Success"; die;
+    		}else{
+    			echo "Failed"; die;
+    		}
+    	}
+    	return view('admin.admin_login');
+    }
+
+    public function dashboard(Request $request){
+        return view('admin.dashboard');
+    }
+}
