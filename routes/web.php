273d91432175ab70cf9752799bc9274e1a388c27
shuvo <shuvosaha.saha87@gmail.com>
2018-09-25 05:45:04 +0600

    Add, Edit or Update and Delete Category and Sub-category. Also view all categories by Admin

--- routes/web.php
+++ routes/web.php
@@ -20,11 +20,18 @@ Route::match(['get','post'], '/admin', 'AdminController@login');
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
 Route::group(['middleware' => ['auth']],function(){
 	Route::get('/admin/dashboard', 'AdminController@dashboard');
 	Route::get('/admin/settings', 'AdminController@settings');
 	Route::get('/admin/check-pwd', 'AdminController@chkPassword');
 	Route::match(['get','post'], '/admin/update-pwd', 'AdminController@updatePassword');
+
+	// Categories Routes (Admin)
+	Route::match(['get','post'], '/admin/add-category','CategoryController@addCategory');
+	Route::match(['get','post'], '/admin/edit-category/{id}','CategoryController@editCategory');
+	Route::match(['get','post'], '/admin/delete-category/{id}','CategoryController@deleteCategory');
+	Route::get('/admin/view-categories', 'CategoryController@viewCategories');
 });
 
 Route::get('/logout', 'AdminController@logout');
