0f74323fd9b49c186704bbb5df3d486638fb9fc8
shuvo <shuvosaha.saha87@gmail.com>
2018-10-02 15:45:33 +0600

    Delete Products | SweetAlert

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -146,6 +146,11 @@ class ProductsController extends Controller
         return view('admin.products.view_products')->with(compact('products'));
     }
 
+    public function deleteProduct($id=null){
+        Product::where(['id'=>$id])->delete();
+        return redirect()->back()->with('flash_message_success', 'Product has been deleted succesfully!');
+    }
+
     public function deleteProductImage($id=null){
         Product::where(['id' => $id])->update(['image' => '']);
         return redirect()->back()->with('flash_message_success', 'Product Image has been deleted succesfully!');
--- public/js/backend_js/matrix.form_validation.js
+++ public/js/backend_js/matrix.form_validation.js
@@ -233,4 +233,34 @@ $(document).ready(function(){
 		return false;
 	});
 
+	/*$("#delProduct").click(function(){
+		//alert("test");
+		if (confirm('Are you sure that you want to delete this Product?')) {
+			return true;
+		}
+		return false;
+	});*/
+
+	$(".deleteRecord").click(function(){
+		var id = $(this).attr('rel');
+		var deleteFunction = $(this).attr('rel1');
+		swal({
+		  title: 'Are you sure?',
+		  text: "You won't be able to revert this!",
+		  type: 'warning',
+		  showCancelButton: true,
+		  confirmButtonColor: '#3085d6',
+		  cancelButtonColor: '#d33',
+		  confirmButtonText: 'Yes, delete it!',
+		  cancelButtonText: 'No, cancel!',
+		  confirmButtonClass: 'btn btn-success',
+		  cancelButtonClass: 'btn btn-danger',
+		  buttonsStyling: false,
+		  reverseButtons: true
+		},
+		function(){
+			window.location.href="/admin/"+deleteFunction+"/"+id;
+		});
+	});
+
 });
