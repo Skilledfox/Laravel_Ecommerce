a05f3f4afe917e9936ef5fac34ebf3efee28210b
shuvo <shuvosaha.saha87@gmail.com>
2018-10-03 20:55:00 +0600

    Show Products in Home Page

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -3,10 +3,21 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Product;
 
 class IndexController extends Controller
 {
     public function index(){
-    	return view('index');
+
+    	// In Ascending Order (By Default)
+    	$productsAll = Product::get();
+
+    	// In Descending Order
+    	$productsAll = Product::orderBy('id', 'DESC')->get();
+
+    	// In Random Order
+    	$productsAll = Product::inRandomOrder()->get();
+
+    	return view('index')->with(compact('productsAll'));
     }
 }
