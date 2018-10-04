275046a111b9deb3d2657a9166e7c09bdc356e95
shuvo <shuvosaha.saha87@gmail.com>
2018-10-04 23:02:12 +0600

    Display Categories at Left Sidebar

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Product;
+use App\Category;
 
 class IndexController extends Controller
 {
@@ -18,6 +19,34 @@ class IndexController extends Controller
     	// In Random Order
     	$productsAll = Product::inRandomOrder()->get();
 
-    	return view('index')->with(compact('productsAll'));
+    	// Get all Categories and Sub Categories
+    	$categories = Category::with('categories')->where(['parent_id' => 0])->get();
+    	/*$categories = json_decode(json_encode($categories));
+    	echo "<pre>"; print_r($categories); die;*/
+
+    	// Without Laravel Relationship
+    	/*$categories_menu = "";
+    	foreach ($categories as $cat) {
+    		$categories_menu .= "<div class='panel-heading'>
+									<h4 class='panel-title'>
+										<a data-toggle='collapse' data-parent='#accordian' href='#".$cat->id."'>
+											<span class='badge pull-right'><i class='fa fa-plus'></i></span>
+											".$cat->name."
+										</a>
+									</h4>
+								</div>
+								<div id='".$cat->id."' class='panel-collapse collapse'>
+									<div class='panel-body'>
+										<ul>";
+										$sub_categories = Category::where(['parent_id' => $cat->id ])->get();
+							    		foreach ($sub_categories as $subcat) {
+							    			$categories_menu .= "<li><a href='".$subcat->url."'>".$subcat->name." </a></li>";
+							    		}
+										$categories_menu .= "</ul>
+									</div>
+								</div>
+								";
+    	}*/
+    	return view('index')->with(compact('productsAll', 'categories_menu', 'categories'));
     }
 }
