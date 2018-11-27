6322058b4a485b9b98704d7fefc5abb80d4bd332
shuvo <shuvosaha.saha87@gmail.com>
2018-11-28 05:08:41 +0600

    Checkout Page | Save Shipping / Delivery Addresses

--- resources/views/products/checkout.blade.php
+++ resources/views/products/checkout.blade.php
@@ -1,9 +1,26 @@
 @extends('layouts.frontLayout.front_design')
 @section('content')
 
-<section id="form"><!--form-->
-		<div class="container">
-			<form action="#">
+<section id="form" style="margin-top: 20px;"><!--form-->
+	<div class="container">
+		<!-- Error Alert for Add Cart -->
+	        @if(Session::has('flash_message_error'))
+	            <div class="alert alert-danger alert-block">
+	                <button type="button" class="close" data-dismiss="alert">×</button> 
+	                <strong>{!! session('flash_message_error') !!}</strong>
+	            </div>
+	        @endif
+	        <!-- End Error Alert for Add Cart -->
+
+	        <!-- Success Alert for Add Cart  -->
+	        @if(Session::has('flash_message_success'))
+	            <div class="alert alert-success alert-block">
+	                <button type="button" class="close" data-dismiss="alert">×</button> 
+	                <strong>{!! session('flash_message_success') !!}</strong>
+	            </div>
+	        @endif
+	        <!-- End Success Alert for Add Cart -->
+		<form action="{{ url('/checkout') }}" method="post">{{ csrf_field() }}
 			<div class="row">
 				<div class="col-sm-4 col-sm-offset-1">
 					<div class="login-form"><!--login form-->
@@ -47,37 +64,37 @@
 					<div class="signup-form"><!--sign up form-->
 						<h2>Ship To</h2>
 							<div class="form-group">
-								<input name="shipping_name" id="shipping_name" type="text" placeholder="Shipping Name" class="form-control" />
+								<input name="shipping_name" id="shipping_name" value="{{ $shippingDetails->name }}" type="text" placeholder="Shipping Name" class="form-control" />
 							</div>
 							<div class="form-group">
-								<input name="shipping_address" id="shipping_address" type="text" placeholder="Shipping Address" class="form-control" />
+								<input name="shipping_address" id="shipping_address" value="{{ $shippingDetails->address }}" type="text" placeholder="Shipping Address" class="form-control" />
 							</div>
 							<div class="form-group">
-								<input name="shipping_city" id="shipping_city" type="text" placeholder="Shipping City" class="form-control" />
+								<input name="shipping_city" id="shipping_city" value="{{ $shippingDetails->city }}" type="text" placeholder="Shipping City" class="form-control" />
 							</div>
 							<div class="form-group">
-								<input name="shipping_state" id="shipping_state" type="text" placeholder="Shipping State" class="form-control" />
+								<input name="shipping_state" id="shipping_state" value="{{ $shippingDetails->state }}" type="text" placeholder="Shipping State" class="form-control" />
 							</div>
 							<div class="form-group">
 								<select id="shipping_country" name="shipping_country" class="form-control">
 									<option value="">Select Country</option>
 									@foreach($countries as $country)
-										<option value="{{ $country->country_name }}">{{ $country->country_name }}</option>
+										<option value="{{ $country->country_name }}"@if($country->country_name == $shippingDetails->country) selected @endif>{{ $country->country_name }}</option>
 									@endforeach
 								</select>
 							</div>
 							<div class="form-group">
-								<input name="shipping_pincode" id="shipping_pincode" type="text" placeholder="Shipping Pincode" class="form-control" />
+								<input name="shipping_pincode" id="shipping_pincode" value="{{ $shippingDetails->pincode }}" type="text" placeholder="Shipping Pincode" class="form-control" />
 							</div>
 							<div class="form-group">
-								<input name="shipping_mobile" id="shipping_mobile" type="text" placeholder="Shipping Mobile" class="form-control" />
+								<input name="shipping_mobile" id="shipping_mobile"  value="{{ $shippingDetails->mobile }}" type="text" placeholder="Shipping Mobile" class="form-control" />
 							</div>
 								<button type="submit" class="btn btn-warning">Checkout</button>
-						</form>
 					</div><!--/sign up form-->
 				</div>
 			</div>
-		</div>
-	</section><!--/form-->
+		</form>
+	</div>
+</section><!--/form-->
 
 @endsection
